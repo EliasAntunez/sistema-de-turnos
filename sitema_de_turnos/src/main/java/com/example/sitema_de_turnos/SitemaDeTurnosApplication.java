ce10ee4f1d3d7466f39cf69fe5be837d9f1ab1ce
Elias E. Antunez <eliasantunez172@gmail.com>
2026-01-03 19:27:38 -0300

    feat/ inicializando el proyecto

---
+++ sitema_de_turnos/src/main/java/com/example/sitema_de_turnos/SitemaDeTurnosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sitema_de_turnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaDeTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaDeTurnosApplication.class, args);
+	}
+
+}
---
+++ sitema_de_turnos/src/test/java/com/example/sitema_de_turnos/SitemaDeTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sitema_de_turnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaDeTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
